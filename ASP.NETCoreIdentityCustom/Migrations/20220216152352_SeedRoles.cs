7cfb136fbf2c208bf091fa63bead35d7c8033af4
Petrit-Gela <petrit.gela@gmail.com>
2022-07-06 11:20:41 +0200

    Commit
    Commit

---
+++ ASP.NETCoreIdentityCustom/Migrations/20220216152352_SeedRoles.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP.NETCoreIdentityCustom.Migrations
+{
+    public partial class SeedRoles : Migration
+    {
+        private string ManagerRoleId = Guid.NewGuid().ToString();
+        private string UserRoleId = Guid.NewGuid().ToString();
+        private string AdminRoleId = Guid.NewGuid().ToString();
+
+        private string User1Id = Guid.NewGuid().ToString();
+        private string User2Id = Guid.NewGuid().ToString();
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SeedRolesSQL(migrationBuilder);
+
+            SeedUser(migrationBuilder);
+
+            SeedUserRoles(migrationBuilder);
+        }
+
+        private void SeedRolesSQL(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@$"INSERT INTO [dbo].[AspNetRoles] ([Id],[Name],[NormalizedName],[ConcurrencyStamp])
+            VALUES ('{AdminRoleId}', 'Administrator', 'ADMINISTRATOR', null);");
+            migrationBuilder.Sql(@$"INSERT INTO [dbo].[AspNetRoles] ([Id],[Name],[NormalizedName],[ConcurrencyStamp])
+            VALUES ('{ManagerRoleId}', 'Manager', 'MANAGER', null);");
+            migrationBuilder.Sql(@$"INSERT INTO [dbo].[AspNetRoles] ([Id],[Name],[NormalizedName],[ConcurrencyStamp])
+            VALUES ('{UserRoleId}', 'User', 'USER', null);");
+        }
+
+        private void SeedUser(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @$"INSERT [dbo].[AspNetUsers] ([Id], [FirstName], [LastName], [UserName], [NormalizedUserName], 
+[Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], 
+[PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) 
+VALUES 
+(N'{User1Id}', N'Petrit', N'Gela', N'petrit.gela@gmail.com', N'PETRIT.GELA@GMAIL.COM', 
+N'petrit.gela@gmail.com', N'PETRIT.GELA@GMAIL.COM', 0, 
+N'AQAAAAEAACcQAAAAEL+1jYbNEmFFm61tVBCZNVdLK0QQZ5Zmq9jXhoI/XyIFQsIgUb2/FtRJvoLMo573HA==', 
+N'HB5265WIAKT7Z2D2MDJVOTUZI4S6Y2H7', N'b0927352-5a08-4222-973b-a732535da1e8', NULL, 0, 0, NULL, 1, 0)");
+
+            migrationBuilder.Sql(
+                @$"INSERT [dbo].[AspNetUsers] ([Id], [FirstName], [LastName], [UserName], [NormalizedUserName], 
+[Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], 
+[PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) 
+VALUES 
+(N'{User2Id}', N'Filan', N'Fisteku ', N'filan.fisteku1@gmail.com', N'FILAN.FISTEKU1@GMAIL.COM', 
+N'filan.fisteku1@gmail.com', N'FILAN.FISTEKU1@GMAIL.COM', 0, 
+N'AQAAAAEAACcQAAAAEAdSdLMFDDaOSNZd0wc/wYTct9dZo+M0pjzH4z+8YhMtR+r+9X6oAtwMGdnlqsur3A==', 
+N'RKNPTBMA5DKSJB7AXLLNJ7CVCW6XXIYK', N'59fd5cb7-51b7-4933-998d-d43d35f20fd1 ', NULL, 0, 0, NULL, 1, 0)");
+        }
+
+        private void SeedUserRoles(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@$"
+        INSERT INTO [dbo].[AspNetUserRoles]
+           ([UserId]
+           ,[RoleId])
+        VALUES
+           ('{User1Id}', '{ManagerRoleId}');
+        INSERT INTO [dbo].[AspNetUserRoles]
+           ([UserId]
+           ,[RoleId])
+        VALUES
+           ('{User1Id}', '{UserRoleId}');");
+
+            migrationBuilder.Sql(@$"
+        INSERT INTO [dbo].[AspNetUserRoles]
+           ([UserId]
+           ,[RoleId])
+        VALUES
+           ('{User2Id}', '{AdminRoleId}');
+        INSERT INTO [dbo].[AspNetUserRoles]
+           ([UserId]
+           ,[RoleId])
+        VALUES
+           ('{User2Id}', '{ManagerRoleId}');");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
